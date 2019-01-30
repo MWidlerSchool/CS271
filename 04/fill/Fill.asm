c3e5b9e61dd7c75b3704f30367974768d305bd59
Michael Widler <WidlerM@OregonState.edu>
2019-01-30 11:26:16 -0800

    Implemented screen manipulation in assembly.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,73 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// calculate size of screen
+(INIT)			// initialize values
+@colorVal
+M = 0
+@WHITE			
+M = 0			// constant for white
+@0
+D = !A
+@BLACK
+M = D			// constant for black
+@8192
+D = A
+@maxIteration
+M = D			// constant for maximum iteration ( = [258 * 512] / 16)
+
+
+(MAIN_LOOP)		// set values for main loop
+@incr			// increment
+M = 0
+@SCREEN
+D = A
+@scrLoc			// scrLoc starts at beginning of screen
+M = D
+
+(IF_KEY_UP)
+@KBD
+D = M			// store keyboard input in data register
+@KEY_UP	
+D; JEQ			// if no key is held, jump to KEY_UP, otherwise will fall into KEY_DOWN
+
+(KEY_DOWN)
+@BLACK
+D = M
+@colorVal
+M = D			// set colorVal to black
+@PAINT_LOOP
+0; JMP			// jump to paint loop
+
+(KEY_UP)
+@WHITE
+D = M
+@colorVal
+M = D			// set colorVal to white, fall off into paint loop
+
+(PAINT_LOOP)		// main loop
+@incr
+D = M
+@maxIteration
+D = D - M
+@MAIN_LOOP
+D; JEQ			// Back to top if incr == 8192
+
+@SCREEN			// load screen address
+D = A			// store in D
+@incr			
+D = D + M		// add increment
+@scrLoc
+M = D			// store to scrLoc
+@colorVal
+D = M			// load colorVal to D
+@scrLoc			// load scrLoc
+A = M			// load stored address as address
+M = D			// paint
+
+@incr
+M = M + 1		// post increment
+
+@PAINT_LOOP
+0; JMP			// jump to PAINT_LOOP
