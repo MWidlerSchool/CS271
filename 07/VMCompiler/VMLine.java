470abff39852753b17683b257be772456e74a7f2
Michael Widler <WidlerM@OregonState.edu>
2019-02-26 11:52:29 -0800

    Realized that two classes were not present in the git repo. Program now passes StackTest.

---
+++ 07/VMCompiler/VMLine.java
@@ -0,0 +1,27 @@
+package VMCompiler;
+
+// a class which represents each line of VM code as either a constant, or two constants and an int
+public class VMLine
+{
+    public final VMConstants arg1;
+    public final VMConstants arg2;
+    public final int arg3;
+    public final int arguments;
+    
+    public VMLine(String str)
+    {
+        String strArr[] = str.split(" ");
+        arguments = strArr.length;
+        arg1 = VMConstants.get(strArr[0]);
+        if(arguments > 1)
+        {
+            arg2 = VMConstants.get(strArr[1]);
+            arg3 = Integer.parseInt(strArr[2]);
+        }
+        else
+        {
+            arg2 = null;
+            arg3 = 0;
+        }
+    }
+}
